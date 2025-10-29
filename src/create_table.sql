fd63acdd327ecd61c35b79c3d3d321fe0a96608b
lokChingt <C24385243@myTUDublin.ie>
2025-10-29 00:17:22 +0000

    chore(db): update tables

--- src/create_table.sql
+++ src/create_table.sql
@@ -1,21 +1,20 @@
-DROP DATABASE IF EXISTS tunes;
+DROP TABLE IF EXISTS tunes;
 CREATE TABLE tunes
 (
     id              INT AUTO_INCREMENT PRIMARY KEY,
-    book            INT,
-    tune_id         VARCHAR(20),
+    book            VARCHAR(10) NOT NULL,
+    tune_id         VARCHAR(20) NOT NULL,
     title           VARCHAR(255),
-    alt_title       VARCHAR(255),
     tune_type       VARCHAR(50),
     key_signature   VARCHAR(20),
     notation        TEXT
 );
 
-DROP DATABASE IF EXISTS tune_alt_titles;
+DROP TABLE IF EXISTS tune_alt_titles;
 CREATE TABLE tune_alt_titles
 (
     id              INT AUTO_INCREMENT PRIMARY KEY,
-    book            INT,
-    tune_id         VARCHAR(20),
+    tune_id         INT,
     alt_title       VARCHAR(255),
+    FOREIGN KEY (tune_id) REFERENCES tunes(id)
 );
