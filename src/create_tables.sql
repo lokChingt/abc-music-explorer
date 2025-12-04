97aaf7f5dd9555132c9b826a97effa2c9639743c
lokChingt <C24385243@myTUDublin.ie>
2025-12-04 11:50:21 +0000

    chore(db): update data type

--- src/create_tables.sql
+++ src/create_tables.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS tunes;
 CREATE TABLE tunes
 (
     id              INT AUTO_INCREMENT PRIMARY KEY,
-    book            VARCHAR(10) NOT NULL,
+    book            INT NOT NULL,
     tune_id         VARCHAR(20) NOT NULL,
     title           VARCHAR(255),
     tune_type       VARCHAR(50),
@@ -18,7 +18,7 @@ DROP TABLE IF EXISTS tune_alt_titles;
 CREATE TABLE tune_alt_titles
 (
     id              INT AUTO_INCREMENT PRIMARY KEY,
-    tune_id         INT,
+    tune_id         INT NOT NULL,
     alt_title       VARCHAR(255),
     FOREIGN KEY (tune_id) REFERENCES tunes(id)
 );
